75a2344282ed3f5070ed7cb1411b5ee099f53c73
harveyspec <harveyspec1245@protonmail.com>
2020-11-02 16:40:06 +0200

    flake8 test

--- countasync.py
+++ countasync.py
@@ -1,13 +1,22 @@
 import asyncio
 
-'''
-trtrtr
-'''
 
-async def count():
+async def count(aaa: int, bbb: int):
+    '''
+
+    Parameters
+    ----------
+    aaa
+    bbb
+
+    Returns
+    -------
+
+    '''
     print("One")
     await asyncio.sleep(1)
     print("Two")
+
 
 async def main():
     await asyncio.gather(count(), count(), count())
@@ -17,9 +26,4 @@ if __name__ == "__main__":
     s = time.perf_counter()
     asyncio.run(main())
     elapsed = time.perf_counter() - s
-    print(f"{__file__} execute d in {elapsed:0.2f} seconds.")
-#test
-#feaure branch
-#test
-#b
-
+    print(f"{__file__} excute d in {elapsed:0.2f} seconds.")
